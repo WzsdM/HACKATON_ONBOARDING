52d35887a01c97394ce0b12ed4f972315f0cc8af
Aniguza <guzmanana0106@gmail.com>
2024-06-08 16:35:55 -0500

    cards

--- juego-memory/juego.php
+++ juego-memory/juego.php
@@ -2,7 +2,7 @@
 <link rel="stylesheet" href="/HACKATON_ONBOARDING/juego-memory/styles.css">
 <div class="modal-dialog  modal-dialog-centered">
     <div class="modal-content  w-auto" id="game">
-        <div class="modal-juego d-flex w-100 align-items-center mb-3">
+        <div class="modal-juego d-flex w-100 align-items-center mb-3"> 
             <h1 class="fs-5 mx-auto" id="staticBackdropLabel">Nivel 1</h1>
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"><i class="ai-cross"></i></button>
         </div>
