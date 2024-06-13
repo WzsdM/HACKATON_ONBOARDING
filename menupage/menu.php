a7013b3713428e5e7e5f0178744995a3cd6c6b50
pyerinaRC28 <pyerinarivdev@gmail.com>
2024-06-13 17:42:05 -0500

    listo

--- menupage/menu.php
+++ menupage/menu.php
@@ -8,7 +8,6 @@
     <link rel="stylesheet" href="menu.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link rel="stylesheet" href="../menupage/style_modals.css">
     <script src="https://unpkg.com/akar-icons-fonts"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
@@ -16,14 +15,6 @@
 </head>
 
 <body>
-    <div class="modalComplete">
-        <div class="modalContainer">
-
-        </div>
-        <div class="modalBackground">
-
-        </div>
-    </div>
     <?php include 'topBar.php';?>
 
     <div class="levelsDispos">
