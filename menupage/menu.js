5763788a931761bed079ada5e0359f433aa7cabe
pyerinaRC28 <pyerinarivdev@gmail.com>
2024-06-13 21:58:37 -0500

    Revert "Merge remote-tracking branch 'origin/menumain'"
    This reverts commit 4801f7907dc0e6260e8dbb33270f917e9fa69c69, reversing
    changes made to c32967c47ad5a4e62f458815e6590ea80d099b64.

--- menupage/menu.js
+++ menupage/menu.js
@@ -38,6 +38,8 @@ function controlarNiveles() {
   const nivelesSuperiores = document.querySelectorAll(".level");
   nivelesSuperiores.forEach((nivel) => {
       const nivelNum = parseInt(nivel.classList[0].replace("level", ""));
+      console.log(nivelUsuario);
+      console.log(nivelNum);
       if (nivelNum == nivelUsuario) { //AQUI YEICOT
         nivel.classList.add("myuserlevel")
           
