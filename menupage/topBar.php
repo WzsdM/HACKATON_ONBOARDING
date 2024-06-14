6771bea66a07cf33cf805028c4e3997be3888744
pyerinaRC28 <pyerinarivdev@gmail.com>
2024-06-14 12:46:34 -0500

    funciona drow

--- menupage/topBar.php
+++ menupage/topBar.php
@@ -5,14 +5,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../menupage/style_modals.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <title>Document</title>
 </head>
 
 <body>
     <div class="topBar">
-        <div class="topBar_left" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+        <div class="topBar_left dropdown" type="button" data-bs-toggle="dropdown" aria-expanded="false">
             <div class="userImg">
                 <img src="../img/perfil.png" alt="User Profile" class="userProfile">
             </div>
@@ -36,9 +37,22 @@
             </div>
         </div>
         <ul class="dropdown-menu dropdown-menu-dark">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+            <li class="d-flex gap-4">
+                <div class="icon"><img src="../img/iconos/icon-perfil.svg"></div>
+                <p>Mi perfil</p>
+            </li>
+            <li class="d-flex gap-4">
+                <div class="icon"><img src="../img/iconos/icon-book.svg"></div>
+                <p>Políticas</p>
+            </li>
+            <li class="d-flex gap-4">
+                <div class="icon"><img src="../img/iconos/icon-support.svg"></div>
+                <p>Soporte</p>
+            </li>
+            <li class="d-flex gap-4" type="button" data-bs-toggle="modal" data-bs-target="#logout">
+                <div class="icon"><img src="../img/iconos/icon-logout.svg"></div>
+                <p>Cerrar Sesión</p>
+            </li>
         </ul>
     </div>
 
